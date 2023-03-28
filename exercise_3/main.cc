2bad36c01b7a1c50388546c8893ab895d89054ba
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-03-28 17:30:47 +0200

    repaired exercise_3

--- exercise_3/main.cc
+++ exercise_3/main.cc
@@ -13,19 +13,20 @@ main(void)
   const double start = omp_get_wtime();
 
   omp_set_num_threads(NUM_OF_THREADS);
-  double sum = 0.0;
+  double total_sum = 0.0;
   #pragma omp parallel
   {
+    double thread_sum = 0.0;
     const int id = omp_get_thread_num();
     for (uint64_t i = id; i < num_steps; i += NUM_OF_THREADS) {
       const double x = (i + 0.5) * step;
-      const double res = 4.0 / (1.0 + x * x);
-  #pragma omp atomic
-      sum += res;
+      thread_sum += 4.0 / (1.0 + x * x);
     }
+  #pragma omp critical
+    total_sum += thread_sum;
   }
 
-  const double pi = step * sum;
+  const double pi = step * total_sum;
   const double total_time = omp_get_wtime() - start;
   std::cout << pi << "\ntime: " << total_time << '\n';
   return EXIT_SUCCESS;
